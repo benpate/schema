8d2d4647b4dfadd98e4a419325fa15b09b5e1e50
Ben Pate <ben@pate.org>
2020-12-11 10:49:23 -0700

    Update unmarshal.go

--- unmarshal.go
+++ unmarshal.go
@@ -4,11 +4,11 @@ import "encoding/json"
 
 // Unmarshal converts a JSON string into a schema.  If the
 // string cannot be converted, then an empty schema is returned.
-func Unmarshal(original string) *Schema {
+func Unmarshal(original string) Schema {
 
 	var result Schema
 
 	json.Unmarshal([]byte(original), &result)
 
-	return &result
+	return result
 }
